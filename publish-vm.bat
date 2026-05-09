bf8f74055e550c7b4a593a727e6a042049a12bae
Artur Klesun <arturklesun@gmail.com>
2026-05-09 16:32:47 +0300

    Add publish-vm.bat to deploy from Mac

--- publish-vm.bat
+++ publish-vm.bat
@@ -11,12 +11,12 @@ if %ERRORLEVEL% neq 0 (
     exit /b %ERRORLEVEL%
 )
 
-call az vm run-command invoke ^
-  --resource-group %RESOURCE_GROUP% ^
-  --name %VM_NAME% ^
-  --command-id RunShellScript ^
-  --scripts "@docker\vm-start.sh" ^
-  --subscription %SUBSCRIPTION% ^
+call az vm run-command invoke \
+  --resource-group DefaultResourceGroup-NEU \
+  --name kunkka-torrent-vm \
+  --command-id RunShellScript \
+  --scripts "@docker\vm-start.sh" \
+  --subscription 3a50dbec-b5b4-4041-b97d-a3125ef48c42 \
   --query "value[0].message" -o tsv
 if %ERRORLEVEL% neq 0 (
     echo VM deploy failed
